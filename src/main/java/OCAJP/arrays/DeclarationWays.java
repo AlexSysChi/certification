344962781c688a3a6325955d7c1e252d783daab3
Alexey.Chistov <alexey.chistov@sidenis.com>
2019-07-01 17:33:24 +0300

    contribution from 01.08.2019

--- src/main/java/OCAJP/arrays/DeclarationWays.java
+++ src/main/java/OCAJP/arrays/DeclarationWays.java
@@ -4,6 +4,7 @@ public class DeclarationWays {
     public static void main(String[] args) {
 
         int[] array = {1, 2, 3}; // such initialization is only possible in declaration line
+        //array = {4, 5, 6}; // won't compile
         array = new int[]{1, 2, 3 ,4};
         array = new int[5];
         //array = new int[5L]; // only int allowed
