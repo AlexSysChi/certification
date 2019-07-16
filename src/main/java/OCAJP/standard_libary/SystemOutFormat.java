e134e236edc55a8ee3be650f1dbe0071240cb06a
Alexey.Chistov <alexey.chistov@sidenis.com>
2019-07-16 16:45:26 +0300

    contribution from 16.08.2019

---
+++ src/main/java/OCAJP/standard_libary/SystemOutFormat.java
@@ -0,0 +1,10 @@
+package OCAJP.standard_libary;
+
+public class SystemOutFormat {
+
+    public static void main(String[] args) {
+        int i = 123;
+        System.out.format("%d", i);
+        System.out.format("%06d", i);
+    }
+}
